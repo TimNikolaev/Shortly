c2e9c325dd576954558998325461b3aec067d032
TimNik <timnikolaev992@gmail.com>
2025-04-26 13:47:27 +0300

    Незначительная правка.

--- cmd/main.go
+++ cmd/main.go
@@ -59,7 +59,9 @@ func main() {
 
 	sqlDB, err := db.DB()
 	if err != nil {
+		logrus.Errorf("failed to get underlying sql.DB: %s", err.Error())
+	}
+	if err := sqlDB.Close(); err != nil {
 		logrus.Errorf("error occurred on db connection close: %s", err.Error())
 	}
-	sqlDB.Close()
 }
